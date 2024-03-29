136071b77157694e850a38a3352087e23cb3d795
Samsaraouo <xmj1175542054@163.com>
2024-03-29 21:05:12 +0800

    changed

--- vue-login/src/utils/http.js
+++ vue-login/src/utils/http.js
@@ -1,1 +1,8 @@
 import axios from 'axios'
+
+axios.defaullts.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+
+const service = axios.create({
+    baseURL:'http://127.0.0.1:8000/api',
+    timeout:15000
+})
